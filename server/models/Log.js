e4aba26448bbd5af15a161a5c060eaa7bbcad3d3
Elena P <elena.pan.ep@gmail.com>
2020-10-23 12:00:47 -0600

    Update Mongoose Schema

--- server/models/Log.js
+++ server/models/Log.js
@@ -6,11 +6,23 @@ const LogSchema = new Schema({
     type: Date,
     required: true
   },
+  // Celsius
   temperature: {
     type: Number,
     required: true
   },
+  // Percentage (0-100)
   moisture: {
+    type: Number,
+    required: true
+  },
+  // Percentage (0-100)
+  humidity: {
+    type: Number,
+    required: true
+  },
+  // 0-100
+  brightness: {
     type: Number,
     required: true
   }
