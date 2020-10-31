fd873c1b18f3d17ad47c3554829f74caa3d0ab81
Elena P <elena.pan.ep@gmail.com>
2020-10-31 16:58:10 -0600

    Add functions to get logs from server

--- app/App.js
+++ app/App.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect, props } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+import { Dimensions } from "react-native";
+const { height } = Dimensions.get("window");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+  const [logs, setLogs] = useState();
+  
+  const getAllLogs = async() => {
+    const url = "http://159.203.41.214:5000/api/logs";
+		try {
+      fetch(url, {method: "GET",})
+        .then((response) => response.json())
+        .then((logs) => {
+          setLogs(logs)
+        })
+		} catch (error) {
+			console.log(error);
+		}
+  }
+  const getRangeLogs = async(start, end) => {
+    const url = `http://159.203.41.214:5000/api/logs/range/${start}/${end}`;
+		try {
+      fetch(url, {method: "GET",})
+        .then((response) => response.json())
+        .then((logs) => {
+          setLogs(logs)
+        })
+		} catch (error) {
+			console.log(error);
+		}
+  }
+  const getOneLog = async(date) => {
+    const url = `http://159.203.41.214:5000/api/logs/${date}`;
+		try {
+      fetch(url, {method: "GET",})
+        .then((response) => response.json())
+        .then((logs) => {
+          setLogs(logs)
+        })
+		} catch (error) {
+			console.log(error);
+		}
+  }
+
+  useEffect(() => {
+		getAllLogs();
+	});
+
+	return (
+		<View>
       <Header />
-    </View>
-  );
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
