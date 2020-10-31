b2d2565abc4d7036c0e643d7c057752c1fc3662f
Elena P <elena.pan.ep@gmail.com>
2020-10-31 16:02:46 -0600

    Add route to get data between two dates

--- server/routes/api/logs.js
+++ server/routes/api/logs.js
@@ -7,9 +7,19 @@ const Log = require("../../models/Log");
 // @access Public
 
 router.get("/:datetime", (req, res) => {
-    Log.findOne({datetime: req.datetime})
+    Log.findOne({datetime: req.params.datetime})
         .then(log => res.json(log))
         .catch(err => res.status(400).json(err));
+});
+
+// @route GET api/logs/:starttime/:endtime
+// @desc Get log between specified datetimes
+// @access Public
+
+router.get("/:starttime/:endtime", (req, res) => {
+  Log.find({datetime: {$gte: req.params.starttime, $lte: req.params.endtime}})
+      .then(logs => res.json(logs))
+      .catch(err => res.status(400).json(err));
 });
 
 // @route GET api/logs
@@ -21,5 +31,24 @@ router.get("/", (req, res) => {
       .catch(err => res.status(400).json(err));
   }
 );
+
+// @route POST api/logs/add
+// @desc Add a log
+// @access Public
+router.post('/add', async (req, res) => {
+  
+  const new_log = await new Log({
+    datetime: new Date(),
+    temperature: 20,
+    moisture: 54,
+    humidity: 65,
+    brightness: 21
+  });
+
+  new_log.save()
+    .then(log => res.json(log))
+    .catch(err => res.status(400).json(err));
+});
+
 
 module.exports = router;
