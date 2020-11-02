85541f3275c9abb4c6d8ce22d64456f9bb78ad4d
Elena P <elena.pan.ep@gmail.com>
2020-11-01 19:43:55 -0700

    Add colors file

---
+++ app/components/Colors.js
@@ -0,0 +1,5 @@
+export const lightGreen1 = "7AFF9F";
+export const lightGreen2 = "73F57D";
+export const lightGreen3 = "61DC82";
+export const darkGreen1 = "62C251";
+export const darkGreen2 = "426B3F";
