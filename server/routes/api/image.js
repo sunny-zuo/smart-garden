adde628638ccc24afcae04a2a672a7dfcf3c7265
Sunny Zuo <me@sunnyzuo.com>
2020-11-07 12:27:20 -0700

    Fix: only require camera when serial is enabled

--- server/routes/api/image.js
+++ server/routes/api/image.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const { takePhoto } = require("../../camera/camera");
 
 // @route GET api/image/latest
 // @desc Get the latest image taken
@@ -13,6 +12,7 @@ router.get("/latest", (req, res) => {
 
 router.get("/take-picture", async (req, res) => {
     if (process.env.ENABLE_SERIAL == "TRUE") {
+        const { takePhoto } = require("../../camera/camera");
         await takePhoto().catch(err => {
             res.status(500).send({ success: false, error: err });
         });
