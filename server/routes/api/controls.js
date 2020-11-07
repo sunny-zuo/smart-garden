e110db47e44a834bb9a9556abbde791f678b747b
Sunny Zuo <me@sunnyzuo.com>
2020-11-06 23:59:59 -0700

    Feat: add response to water API route

--- server/routes/api/controls.js
+++ server/routes/api/controls.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { waterPlant } = require("../../serial");
+
 
 // @route POST api/controls/water
 // @desc Water plant
 // @access Public
 router.post('/water', (req, res) => {
   // const amount = req.body.amount;
-  waterPlant();
+  if (process.env.ENABLE_SERIAL === "TRUE") {
+    const { waterPlant } = require("../../serial");
+    waterPlant();
+  }
+  res.send({ success: true });
 });
 
 
