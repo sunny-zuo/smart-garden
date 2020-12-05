f96f0060d949560ea9032a384ed3704c0828a301
Umut Can Emre <umutcanemre@hotmail.ca>
2020-12-05 16:06:11 -0700

    cite a source in arduino code

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -2,13 +2,13 @@
 // Written by ladyada, public domain
 
 // REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library (I also use pieces of example code from this library)
 // - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
 
 #include "DHT.h"
 
-#define DHTPIN 2     
-#define DHTTYPE DHT11   
+#define DHTPIN 2
+#define DHTTYPE DHT11
 
 int pumpRelayPin = 4;
 //int relayPin2 = 5;
@@ -29,10 +29,10 @@ String incomingByte;
 
 bool pumping;
 
-typedef struct tempHumidityReading  { 
+typedef struct tempHumidityReading  {
 
   float temp,humidity;
-  
+
 } tempHumidityReading;
 
 float normalizeMoistureReading(float reading) {
@@ -50,7 +50,7 @@ void setup() {
 
 float getMoistureReading() {
   float moistureReading = analogRead(MOISTUREPIN);
-  
+
   if (isnan(moistureReading)) {
     Serial.println(F("Failed to read from Moisture sensor!"));
     return (float) -100; //random error will catch our attention server side
@@ -63,27 +63,27 @@ float getMoistureReading() {
 
 void loop() {
   // Wait a few seconds between measurements.
-  
+
   if ((unsigned long) (millis() - time_now) > measurementInterval) {
     time_now = millis();
 
     float m = getMoistureReading();
-    
+
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
 
     float h = dht.readHumidity();
     // Read temperature as Celsius (the default)
     float t = dht.readTemperature();
-    
+
     // Check if any reads failed and exit early (to try again).
     if (isnan(h) || isnan(t)) {
       Serial.println(F("Failed to read from DHT sensor!"));
       return;
     }
-    
+
     Serial.print(F("moisture: "));
-    Serial.println(m);  
+    Serial.println(m);
     Serial.print(F("humidity: "));
     Serial.println(h);
     Serial.print(F("temperature: "));
@@ -95,7 +95,7 @@ void loop() {
     incomingByte = Serial.readString();
     pumping = true;
     pump_now = millis();
-  
+
     digitalWrite(pumpRelayPin, LOW); //turn pump switch ON
 
     //debugging, will mess with server in current state
@@ -106,10 +106,10 @@ void loop() {
   if (pumping && ((unsigned long) (millis() - pump_now) > pumpDuration)){
     pumping = false;
   }
-  
+
   else if (!pumping) {
     digitalWrite(pumpRelayPin, HIGH); //turn pump switch OFF
   }
-  
+
 }
- 
+
