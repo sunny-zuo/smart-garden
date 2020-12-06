285e01f30bab0965fe2d63a6a2a32299a3791a5d
Sunny Zuo <me@sunnyzuo.com>
2020-12-05 19:59:18 -0700

    feat: render real data in UI

--- app/components/PlantData.js
+++ app/components/PlantData.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ProgressCircle from 'react-native-progress-circle'
 import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import DataDisplay from './DataDisplay';
@@ -6,6 +6,13 @@ import { darkGreen1, darkGreen2, white } from './Colors';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function PlantData() {
+    const [logs, setLogs] = useState();
+    const [lastLog, setLastLog] = useState();
+
+    useEffect(() => {
+        getAllLogs();
+    }, []);
+
     const waterPlant = () => {
         const url = `http://10.0.0.11:5000/api/controls/water`;
         try {
@@ -30,6 +37,31 @@ export default function PlantData() {
             console.log(error);
         }
     }
+    
+    const getAllLogs = async () => {
+        const url = "http://159.203.41.214:5000/api/logs";
+        try {
+            fetch(url, { method: "GET", })
+                .then((response) => response.json())
+                .then((logs) => {
+                    setLogs(logs);
+                    setLastLog(logs.slice(-1)[0]);
+                })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // function that returns a health percentage from a given log. Fairly simple at the moment.
+    const calculateHealth = (log) => {
+        const tempPercent = (15 <= log.temperature && log.temperature <= 30) ? 1 : 1 / Math.abs(log.temperature - 22) * 7;
+        const moisturePercent = log.moisture;
+        return (tempPercent + moisturePercent * 2) * 100 / 3;
+    }
+
+    if (!logs || !lastLog) {
+        return null;
+    }
 
     return (
         <View style={styles.card}>
@@ -37,9 +69,9 @@ export default function PlantData() {
             <View style={styles.majorDataView}>
                 <View style={{ flex: 0.5, alignItems: 'center' }}>
                     <View>
-                        <ProgressCircle percent={86} radius={50} borderWidth={4} color={darkGreen1} shadowColor={"#CDCDCD"} bgColor={white}>
+                        <ProgressCircle percent={calculateHealth(lastLog).toFixed(0)} radius={50} borderWidth={4} color={darkGreen1} shadowColor={"#CDCDCD"} bgColor={white}>
                             <Text style={{ color: darkGreen1 }}>
-                                <Text style={{ fontSize: 30 }}>{'86'}</Text><Text style={{ fontSize: 14 }}>%</Text>
+                                <Text style={{ fontSize: 30 }}>{calculateHealth(lastLog).toFixed(0)}</Text><Text style={{ fontSize: 14 }}>%</Text>
                             </Text>
                             <Text style={{ color: darkGreen1, fontSize: 10 }}>
                                 HEALTH
@@ -64,10 +96,10 @@ export default function PlantData() {
                     </View>
                 </View>
                 <View style={{ flex: 0.5, paddingLeft: 40 }}>
-                    <DataDisplay icon={<FontAwesome5 name="ruler-vertical" size={16} color={'#000000'}/>} type={'Height'} value={'0.324'} unit={'m'}/>
-                    <DataDisplay icon={<FontAwesome5 name="thermometer-quarter" size={16} color={'#D50000'} />} type={'Temperature'} value={'23'} unit={'°C'} />
-                    <DataDisplay icon={<FontAwesome5 name="tint" size={16} color={'#03A9F4'} />} type={'Moisture'} value={'85'} unit={'%'} />
-                    <DataDisplay icon={<FontAwesome5 name="lightbulb" size={16} color={'#FBC02D'} />} type={'Light'} value={'69'} unit={'%'} />
+                    <DataDisplay icon={<FontAwesome5 name="ruler-vertical" size={16} color={'#000000'} />} type={'Height'} value={(lastLog.height * 100).toFixed(1)} unit={'m'}/>
+                    <DataDisplay icon={<FontAwesome5 name="thermometer-quarter" size={16} color={'#D50000'} />} type={'Temperature'} value={lastLog.temperature.toFixed(1)} unit={'°C'} />
+                    <DataDisplay icon={<FontAwesome5 name="tint" size={16} color={'#03A9F4'} />} type={'Moisture'} value={(lastLog.moisture * 100).toFixed(1)} unit={'%'} />
+                    <DataDisplay icon={<FontAwesome5 name="lightbulb" size={16} color={'#FBC02D'} />} type={'Light'} value={(lastLog.brightness * 100).toFixed(1)} unit={'%'} />
                 </View>
             </View>
         </View>
