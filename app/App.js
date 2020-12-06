7b7a0defef5e26ee1f4939c65ed1f398ae1daee0
Sunny Zuo <me@sunnyzuo.com>
2020-12-05 18:23:02 -0700

    feat: current variable display

--- app/App.js
+++ app/App.js
@@ -99,147 +99,15 @@ function HomeScreen() {
   );
 }
 
-function HeightScreen() {
-  const [logs, setLogs] = useState();
-
-  const getAllLogs = async() => {
-    const url = "http://159.203.41.214:5000/api/logs";
-		try {
-      fetch(url, {method: "GET",})
-        .then((response) => response.json())
-        .then((logs) => {
-          setLogs(logs)
-        })
-		} catch (error) {
-			console.log(error);
-		}
-  }
-  const getRangeLogs = async(start, end) => {
-    const url = `http://159.203.41.214:5000/api/logs/range/${start}/${end}`;
-		try {
-      fetch(url, {method: "GET",})
-        .then((response) => response.json())
-        .then((logs) => {
-          setLogs(logs)
-        })
-		} catch (error) {
-			console.log(error);
-		}
-  }
-  const getOneLog = async(date) => {
-    const url = `http://159.203.41.214:5000/api/logs/${date}`;
-		try {
-      fetch(url, {method: "GET",})
-        .then((response) => response.json())
-        .then((logs) => {
-          setLogs(logs)
-        })
-		} catch (error) {
-			console.log(error);
-		}
-  }
-  const takePicture = () => {
-    const url = `http://10.0.0.11:5000/api/image/take-picture`;
-		try {
-      fetch(url, {method: "POST",})
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json);
-        })
-		} catch (error) {
-			console.log(error);
-		}
-  }
-  const formatDate = date => {
-    const hour = new Date(date).getHours();
-    const minutes = new Date(date).getMinutes();
-    return `${hour}:${minutes}`;
-  }
-
-  useEffect(() => {
-    getAllLogs();
-	}, []);
-
-  if (logs == null) {
-    return null;
-  }
-  return (
-    <SafeAreaView style = {styles.container}>
-    <ScrollView style={styles.scrollView}>
-      <Header />
-      <View style={{ marginTop: 20,  marginLeft: 100, marginRight: 100, height: 1}}>
-        <Button
-        color = 'lightgreen'
-          onPress={() => takePicture()}
-          title="Take Picture"/>
-      </View>
-      <View style={{ alignItems: 'center', height: 300 }}>
-          <Image source={{ uri: 'http://159.203.41.214:5000/api/image/latest' }} style={{ width: '90%', height: undefined, aspectRatio: 1, resizeMode: 'contain', borderRadius: 10}}></Image>
-      </View>
-      <GraphHeader title = 'Height over time: '/>
-      <LineChart
-        data={{
-          labels: ["24 Hours Ago","16 Hours Ago","8 Hours Ago",],
-          datasets: [
-            {
-              data: logs.map(log => log.temperature).slice(-144)
-            }
-          ]
-        }}
-        width={Dimensions.get("window").width} // from react-native
-        height={220}
-        yAxisSuffix="cm"
-        yAxisInterval={100} // optional, defaults to 1
-        fromZero={true}
-        chartConfig={{
-          backgroundColor: lightGreen1,
-          backgroundGradientToOpacity: 0.4,
-          backgroundGradientFromOpacity: 0.4,
-          backgroundGradientFrom: lightGreen1,
-          backgroundGradientTo: lightGreen1,
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16
-          },
-          propsForDots: {
-            r: "2",
-            strokeWidth: "2",
-            stroke: darkGreen2
-          }
-        }}
-        bezier
-        style={{
-          marginVertical: 5,
-          borderRadius: 16
-        }}
-      />
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
 export default function App() {
 
   return (
-    <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Height Data" component={HeightScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <ImageHeader />
+        <PlantData />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- app/components/PlantData.js
+++ app/components/PlantData.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import ProgressCircle from 'react-native-progress-circle'
 import { Text, View, Image, StyleSheet } from 'react-native';
-import { darkGreen1, white } from './Colors';
+import DataDisplay from './DataDisplay';
+import { darkGreen1, darkGreen2, white } from './Colors';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const PlantData = () => {
     return (
         <View style={styles.card}>
             <Text style={styles.title}>Garlic Plant</Text>
             <View style={styles.majorDataView}>
-                <View style={{ flex: 0.35, alignItems: 'center' }}>
-                    <ProgressCircle percent={30} radius={50} borderWidth={4} color={darkGreen1} shadowColor={"#CDCDCD"} bgColor={white}>
+                <View style={{ flex: 0.5, alignItems: 'center' }}>
+                    <ProgressCircle percent={86} radius={50} borderWidth={4} color={darkGreen1} shadowColor={"#CDCDCD"} bgColor={white}>
                         <Text style={{ color: darkGreen1 }}>
-                            <Text style={{ fontSize: 30 }}>{'30'}</Text><Text style={{ fontSize: 14 }}>%</Text>
+                            <Text style={{ fontSize: 30 }}>{'86'}</Text><Text style={{ fontSize: 14 }}>%</Text>
                         </Text>
                         <Text style={{ color: darkGreen1, fontSize: 10 }}>
                             HEALTH
                         </Text>
                     </ProgressCircle>
                 </View>
-                <View style={{ flex: 0.65 }}>
-
+                <View style={{ flex: 0.5, paddingLeft: 40 }}>
+                    <DataDisplay icon={<FontAwesome5 name="ruler-vertical" size={16} color={'#000000'}/>} type={'Height'} value={'0.324'} unit={'m'}/>
+                    <DataDisplay icon={<FontAwesome5 name="thermometer-quarter" size={16} color={'#D50000'} />} type={'Temperature'} value={'23'} unit={'°C'} />
+                    <DataDisplay icon={<FontAwesome5 name="tint" size={16} color={'#03A9F4'} />} type={'Moisture'} value={'85'} unit={'%'} />
+                    <DataDisplay icon={<FontAwesome5 name="lightbulb" size={16} color={'#FBC02D'} />} type={'Light'} value={'69'} unit={'%'} />
                 </View>
             </View>
         </View>
@@ -44,7 +49,7 @@ const styles = StyleSheet.create({
         fontSize: 26,
         textAlign: 'left',
         fontWeight: 'bold',
-        paddingLeft: 10,
+        paddingLeft: 14,
         paddingTop: 4,
     },
     majorDataView: {
