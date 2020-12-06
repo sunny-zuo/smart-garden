1e5b313b7a2f96051f8d16954c8edb8db137e509
Sunny Zuo <me@sunnyzuo.com>
2020-12-05 19:03:04 -0700

    fix: image caching

--- app/components/ImageHeader.js
+++ app/components/ImageHeader.js
@@ -4,7 +4,9 @@ import { View, Image, StyleSheet } from 'react-native';
 const ImageHeader = () => {
     return (
         <View>
-            <Image style={styles.image} source={{ uri: 'http://159.203.41.214:5000/api/image/latest' }} />
+            {/* We add a Math.random() to the end of the uri as a query string to change the URL and force react native to reload the image instead of caching. 
+                See https://github.com/facebook/react-native/issues/12606 */}
+            <Image style={styles.image} source={{ uri: 'http://159.203.41.214:5000/api/image/latest' + '?'+ Math.random() }} />
         </View>
     );
 };
